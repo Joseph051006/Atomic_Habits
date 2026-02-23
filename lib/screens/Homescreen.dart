068f780360de50f7871cf03b0d4bc03a25d810a7
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-02-23 09:09:46 +0100

    Minor Changes

--- lib/screens/Homescreen.dart
+++ lib/screens/Homescreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../widgets/AppBar/HomeScreenAppBar.dart';
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
