396e047b6996fc2bd4861dcc650aa1e9cd66bf87
Joseph <167524188+Joseph051006@users.noreply.github.com>
2026-03-29 15:27:37 +0200

    Created Timer
    Created the nessecary timer for the 2 Minutes.

--- lib/screens/TwoMinutes.dart
+++ lib/screens/TwoMinutes.dart
@@ -11,21 +11,64 @@ class Twominutes extends StatefulWidget {
 }
 
 class _TwominutesState extends State<Twominutes> {
+  late Timer _timer;
+  int _elapsedTime = 0;
+  bool _isRunning = false;
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startTimer(){
+    if (!_isRunning){
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) =>
+        setState(() {
+          _elapsedTime++;
+        })
+      );
+    }
+    setState(() {
+      _isRunning = true;
+    });
+  }
+
+  void _stopTimer(){
+    _timer.cancel();
+    setState(() {
+      _isRunning = false;
+    });
+  }
+
+  void _resetTimer(){
+    _timer.cancel();
+    setState(() {
+      _elapsedTime = 0;
+      _isRunning = false;
+    });
+  }
+
+  String _formatTime(){
+    int minutes = _elapsedTime ~/ 60;
+    int seconds = _elapsedTime % 60;
+    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';    
+  }
+
   @override
   Widget build(BuildContext context) {
     
     return Column(
       children: [
         Center(
-          child: Text("Display time"),
+          child: Text(_formatTime()),
         ),
         SizedBox(),
         Row(
           children: [
-            ElevatedButton(onPressed: null, child: Text("Start")),
-            SizedBox(),
-            ElevatedButton(onPressed: null, child: Text("Stop")),
-            ElevatedButton(onPressed: null, child: Text("Reset"))
+            ElevatedButton(onPressed: _startTimer, child: Text("Start")),
+            ElevatedButton(onPressed: _stopTimer, child: Text("Stop")),
+            ElevatedButton(onPressed: _resetTimer, child: Text("Reset"))
           ],
         ), 
       ],
